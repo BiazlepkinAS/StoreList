83733b255426cb51424de713b5ceeba319652a97
Andrei Bezlepkin <6abez6@gmail.com>
2023-08-03 17:41:58 +0300

    Initial commit

---
+++ StoreList/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  StoreList
+//
+//  Created by Andrei Bezlepkin on 3.08.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+        configUI()
+    }
+    
+    private func configUI() {
+        print("SList")
+    }
+
+}
+
